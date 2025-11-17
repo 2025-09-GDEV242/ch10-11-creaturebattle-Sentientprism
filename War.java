099e3cbe66afb2864181779a56bf7dd8cda62123
Sentientprism <doughertyedmund7@gmail.com>
2025-11-17 15:07:22 -0500

    good and evil armies generate when a war is created

--- War.java
+++ War.java
@@ -1,4 +1,3 @@
-
 import java.util.ArrayList;
 /**
  * Write a description of class War here.
@@ -8,25 +7,79 @@ import java.util.ArrayList;
  */
 public class War
 {
-    public ArrayList<Creature> goodArmy = new ArrayList<Creature>();
-    public ArrayList<Creature> evilArmy = new ArrayList<Creature>();
+    ArrayList<Creature> goodArmy = new ArrayList<Creature>();
+    ArrayList<Creature> evilArmy = new ArrayList<Creature>();
+    public Creature goodWarrior;
+    public Creature evilWarrior;
 
     /**
      * Constructor for objects of class War
      */
     public War()
     {
-        int randomNumber; //holds results from randomizer
+        initializeGoodArmy();
+        initializeEvilArmy();
+        goodWarrior = goodArmy.get(goodArmy.size() - 1);
+        evilWarrior = evilArmy.get(evilArmy.size() - 1);
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
+    
+    public void fight()
     {
-        return 5;
+        
+    }
+    
+    private void initializeGoodArmy()
+    {
+        int randomNumber;
+        while (goodArmy.size() < 100)
+        {
+            randomNumber = Randomizer.nextInt(100);
+            if (randomNumber > 94)
+            {
+                goodArmy.add(new Biscuit());
+                continue;
+            }
+            else if (randomNumber > 50)
+            {
+                goodArmy.add(new Elf());
+                continue;
+            }
+            else if (randomNumber > 30)
+            {
+                goodArmy.add(new Krogan());
+                continue;
+            }
+            else
+            {
+                goodArmy.add(new Human());
+                continue;
+            }
+            
+        }
+    }
+    
+    private void initializeEvilArmy()
+    {
+        int randomNumber;
+        while (evilArmy.size() < 100)
+        {
+            randomNumber = Randomizer.nextInt(100);
+            if (randomNumber > 80)
+            {
+                evilArmy.add(new Balrog());
+                continue;
+            }
+            else if (randomNumber > 50)
+            {
+                evilArmy.add(new Cyberdemon());
+                continue;
+            }
+            else
+            {
+                evilArmy.add(new Krogan());
+                continue;
+            }
+            
+        }
     }
 }
